9b55a7a9152d338d1627c5612a1486625a67cd52
epg2xml <epg3xml@gmail.com>
2022-04-01 11:56:55 +0900

    Start v2.1.0-DEV

--- epg2xml/__init__.py
+++ epg2xml/__init__.py
@@ -1,5 +1,5 @@
 __title__ = "epg2xml"
 __description__ = "웹 상의 소스를 취합하여 EPG를 만드는 프로그램"
 __url__ = "https://github.com/epg2xml/epg2xml"
-__version__ = "2.0.0"
+__version__ = "2.1.0-DEV"
 __license__ = "BSD 3-Clause"
