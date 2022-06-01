1738010fafb562fd174f22053d5550c30c67e7a7
epg2xml <epg3xml@gmail.com>
2022-06-01 18:02:13 +0900

    kt: fix request params

--- epg2xml/providers/kt.py
+++ epg2xml/providers/kt.py
@@ -10,13 +10,22 @@ log = logging.getLogger(__name__.rsplit(".", maxsplit=1)[-1].upper())
 
 
 class KT(EPGProvider):
+    """EPGProvider for KT
+
+    데이터: rawhtml
+    요청수: #channels * #days
+    특이사항:
+    - 가끔 업데이트 지연
+    - 프로그램 시작 시각만 제공
+    """
+
     referer = "https://tv.kt.com/"
     no_endtime = True
 
     def get_svc_channels(self):
         url = "https://tv.kt.com/tv/channel/pChList.asp"
         params = {"ch_type": "1", "parent_menu_id": "0"}
-        soup = BeautifulSoup(self.request(url, method="POST", params=params))
+        soup = BeautifulSoup(self.request(url, method="POST", data=params))
         raw_channels = [unquote(x.find("span", {"class": "ch"}).text.strip()) for x in soup.select("li > a")]
         # 몇몇 채널은 (TV로만 제공, 유료채널) 웹에서 막혀있지만 실제로는 데이터가 있을 수 있다.
         self.svc_channel_list = [
@@ -37,7 +46,7 @@ class KT(EPGProvider):
                 day = date.today() + timedelta(days=nd)
                 params.update({"service_ch_no": _ch.svcid, "seldate": day.strftime("%Y%m%d")})
                 try:
-                    data = self.request(url, method="POST", params=params)
+                    data = self.request(url, method="POST", data=params)
                     soup = BeautifulSoup(data, parse_only=SoupStrainer("tbody"))
                     for row in soup.find_all("tr"):
                         cell = row.find_all("td")
