64bed181975dbf52bc9bfc7edfcdf6bae1703a62
epg2xml <epg3xml@gmail.com>
2022-09-20 14:54:00 +0900

    test: sk 제거
    * github-hosted runners로부터 접속이 원활하지 않아 테스트 불가

--- epg2xml/__main__.py
+++ epg2xml/__main__.py
@@ -117,6 +117,8 @@ def main():
 if __name__ == "__main__":
     try:
         main()
+    except KeyboardInterrupt:
+        sys.exit(0)
     except Exception as _e:
         log.exception(_e)
         sys.exit(1)
