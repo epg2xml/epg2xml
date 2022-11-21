epg2xml/epg2xml

commit 2ed1ec20564fbea3bc24f6618615e20e20c7c640

refactor: setup root logger in init

epg2xml/config.py
@@ -6,11 +6,38 @@ import sys
 import errno
 from copy import copy
 from pathlib import Path
+from typing import Union
 
 from epg2xml.utils import dump_json
 from epg2xml import __version__, __title__, __description__, __url__
 
+# suppress modules logging
+logging.getLogger("requests").setLevel(logging.ERROR)
+logging.getLogger("urllib3.connectionpool").setLevel(logging.ERROR)
+
 logger = logging.getLogger("CONFIG")
+
+
+def setup_root_logger(
+    *,
+    handler: logging.Handler = None,
+    formatter: logging.Formatter = None,
+    level: Union[int, str] = None,
+) -> None:
+    if level is None:
+        level = logging.INFO
+
+    if handler is None:
+        # Console logger, log to stdout instead of stderr
+        handler = logging.StreamHandler(sys.stdout)
+
+    if formatter is None:
+        log_fmt = "%(asctime)-15s %(levelname)-8s %(name)-7s %(lineno)4d: %(message)s"
+        formatter = logging.Formatter(log_fmt, datefmt="%Y/%m/%d %H:%M:%S")
+
+    handler.setFormatter(formatter)
+    logging.getLogger().addHandler(handler)
+    logging.getLogger().setLevel(level)
 
 
 class Singleton(type):

epg2xml/__main__.py
@@ -3,10 +3,9 @@ import json
 import socket
 import logging
 from contextlib import ExitStack
-from logging.handlers import RotatingFileHandler
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-from epg2xml.config import Config
+from epg2xml.config import Config, setup_root_logger
 from epg2xml.providers import load_providers, load_channels
 from epg2xml import __version__, __title__
 
@@ -15,39 +14,26 @@ from epg2xml import __version__, __title__
 ############################################################
 
 # logging
-log_fmt = "%(asctime)-15s %(levelname)-8s %(name)-7s %(lineno)4d: %(message)s"
-formatter = logging.Formatter(log_fmt, datefmt="%Y/%m/%d %H:%M:%S")
-rootLogger = logging.getLogger()
-rootLogger.setLevel(logging.INFO)
-
-# suppress modules logging
-logging.getLogger("requests").setLevel(logging.ERROR)
-logging.getLogger("urllib3.connectionpool").setLevel(logging.ERROR)
-
-# logging to console, stderr by default
-consolehandler = logging.StreamHandler()
-consolehandler.setFormatter(formatter)
-rootLogger.addHandler(consolehandler)
+setup_root_logger()
 
 # load initial config
 conf = Config()
 
 if conf.settings["logfile"] is not None:
+    from logging.handlers import RotatingFileHandler
+
     # logging to file
-    filehandler = RotatingFileHandler(
-        conf.settings["logfile"], maxBytes=1024 * 1024 * 2, backupCount=5, encoding="utf-8"
-    )
-    filehandler.setFormatter(formatter)
-    rootLogger.addHandler(filehandler)
+    fileHandler = RotatingFileHandler(conf.settings["logfile"], maxBytes=2 * 1024**2, backupCount=5, encoding="utf-8")
+    setup_root_logger(handler=fileHandler)
 
 # set configured log level
-rootLogger.setLevel(conf.settings["loglevel"])
+logging.getLogger().setLevel(conf.settings["loglevel"])
 
 # load config file
 conf.load()
 
 # logger
-log = rootLogger.getChild("MAIN")
+log = logging.getLogger("MAIN")
 
 ############################################################
 # MAIN

commit 6cc983f443c220039915924e93abf472c4177aa8

fix corrupted stdout by logging - fixes #27
* this issue is introduced during the refactoring in 2ed1ec20564fbea3bc24f6618615e20e20c7c640

epg2xml/config.py
@@ -28,8 +28,8 @@ def setup_root_logger(
         level = logging.INFO
 
     if handler is None:
-        # Console logger, log to stdout instead of stderr
-        handler = logging.StreamHandler(sys.stdout)
+        # logging to console, stderr by default
+        handler = logging.StreamHandler()
 
     if formatter is None:
         log_fmt = "%(asctime)-15s %(levelname)-8s %(name)-7s %(lineno)4d: %(message)s"