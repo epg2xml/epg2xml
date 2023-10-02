fcdde19c183edcafb9328c8fa1ff39d50127164d
epg2xml <80042145+epg2xml@users.noreply.github.com>
2023-10-03 00:56:57 +0900

    wavve: API 변경 대응 (#38)
    * __url()과 __params()를 도입하여 API 요청을 단순화
    * header wavve-credential 추가
    * header referer 하나로 고정
    * programdetail: dictcache 대신 functools.lru_cache 도입
    * style: 개별 프로그램 가져오는 부분 분리

--- epg2xml/providers/wavve.py
+++ epg2xml/providers/wavve.py
@@ -1,9 +1,9 @@
 import logging
+from datetime import date, datetime, timedelta
+from functools import lru_cache
 from xml.sax.saxutils import unescape
-from datetime import datetime, timedelta, date
 
-from epg2xml.providers import EPGProvider, EPGProgram
-from epg2xml.utils import ua, request_data
+from epg2xml.providers import EPGProgram, EPGProvider
 
 log = logging.getLogger(__name__.rsplit(".", maxsplit=1)[-1].upper())
 today = date.today()
@@ -14,59 +14,114 @@ class WAVVE(EPGProvider):
 
     데이터: jsonapi
     요청수: 1
+    특이사항:
+    - 해외나 VPS는 차단 가능성이 높음
     """
 
-    referer = "https://www.wavve.com/schedule/index.html"
+    referer = "https://www.wavve.com/"
     title_regex = r"^(.*?)(?:\s*[\(<]?([\d]+)회[\)>]?)?(?:\([월화수목금토일]?\))?(\([선별전주\(\)재방]*?재[\d방]?\))?\s*(?:\[(.+)\])?$"
-    url = "https://apis.wavve.com/live/epgs"
-    params = {
-        "enddatetime": "2020-01-20 24:00",
-        "genre": "all",
-        "limit": 200,
-        "offset": 0,
-        "startdatetime": "2020-01-20 21:00",
+    base_url = "https://apis.wavve.com"
+    base_params = {
         "apikey": "E5F3E0D30947AA5440556471321BB6D9",
-        "credential": "none",
+        "client_version": "6.0.1",
         "device": "pc",
         "drm": "wm",
         "partner": "pooq",
         "pooqzone": "none",
         "region": "kor",
-        "targetage": "auto",
+        "targetage": "all",
     }
     no_endtime = False
 
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        self.sess.headers.update({"wavve-credential": "none"})
+
+    def __url(self, url: str) -> str:
+        """completes partial urls from api response or for api request"""
+        if url.startswith(("http://", "https://")):
+            return url
+        if url.startswith("/"):
+            return self.base_url + url
+        return "https://" + url
+
+    def __params(self, **params) -> dict:
+        """returns url parameters for api requests with base ones"""
+        p = self.base_params.copy()
+        p.update(params)
+        return p
+
+    def __get(self, url: str, **kwargs):
+        url = self.__url(url)
+        params = self.__params(**kwargs.pop("params", {}))
+        return self.request(url, params=params, **kwargs)
+
     def get_svc_channels(self):
-        # update parameters for requests
         today_str = today.strftime("%Y-%m-%d")
         hour_min = datetime.now().hour // 3
-        self.params.update(
-            {
-                "startdatetime": f"{today_str} {hour_min*3:02d}:00",
-                "enddatetime": f"{today_str} {(hour_min+1)*3:02d}:00",
-            }
-        )
+        # 현재 시간과 가까운 미래에 서비스 가능한 채널만 가져옴
+        params = {
+            "enddatetime": f"{today_str} {(hour_min+1)*3:02d}:00",
+            "genre": "all",
+            "limit": 500,
+            "offset": 0,
+            "startdatetime": f"{today_str} {hour_min*3:02d}:00",
+        }
         self.svc_channel_list = [
             {
                 "Name": x["channelname"],
-                "Icon_url": "https://" + x["channelimage"],
+                "Icon_url": self.__url(x["channelimage"]),
                 "ServiceId": x["channelid"],
             }
-            for x in self.request(self.url, params=self.params)["list"]
+            for x in self.__get("/live/epgs", params=params)["list"]
         ]
 
+    def __program(self, channelid: str, program: dict) -> EPGProgram:
+        _prog = EPGProgram(channelid)
+        _prog.stime = datetime.strptime(program["starttime"], "%Y-%m-%d %H:%M")
+        _prog.etime = datetime.strptime(program["endtime"], "%Y-%m-%d %H:%M")
+        _prog.title = unescape(program["title"])
+        matches = self.title_regex.match(_prog.title)
+        if matches:
+            _prog.title = (matches.group(1) or "").strip()
+            _prog.title_sub = (matches.group(4) or "").strip()
+            episode = (matches.group(2) or "").replace("회", "").strip()
+            _prog.ep_num = "" if episode == "0" else episode
+            _prog.rebroadcast = bool(matches.group(3))
+        _prog.rating = 0 if program["targetage"] == "n" else int(program["targetage"])
+
+        # 추가 정보 가져오기
+        if not self.cfg["GET_MORE_DETAILS"]:
+            return _prog
+        programid = program["programid"].strip()
+        if not programid:
+            # 개별 programid가 없는 경우도 있으니 체크해야함
+            return _prog
+        programdetail = self.get_program_details(programid)
+        if not programdetail:
+            return _prog
+        # programtitle = programdetail['programtitle']
+        # log.info('%s / %s' % (programName, programtitle))
+        _prog.desc = "\n".join(
+            [x.replace("<br>", "\n").strip() for x in programdetail["programsynopsis"].splitlines()]
+        )  # carriage return(\r) 제거, <br> 제거
+        _prog.category = programdetail["genretext"].strip()
+        _prog.poster_url = self.__url(programdetail["programposterimage"].strip())
+        # tags = programdetail['tags']['list'][0]['text']
+        if programdetail["actors"]["list"]:
+            _prog.actors = [x["text"] for x in programdetail["actors"]["list"]]
+        return _prog
+
     def get_programs(self, lazy_write=False):
-        # for caching program details
-        programcache = {}
-        # update parameters for requests
-        self.params.update(
-            {
-                "startdatetime": today.strftime("%Y-%m-%d") + " 00:00",
-                "enddatetime": (today + timedelta(days=int(self.cfg["FETCH_LIMIT"]) - 1)).strftime("%Y-%m-%d")
-                + " 24:00",
-            }
-        )
-        channeldict = {x["channelid"]: x for x in self.request(self.url, params=self.params)["list"]}
+        # parameters for requests
+        params = {
+            "enddatetime": (today + timedelta(days=int(self.cfg["FETCH_LIMIT"]) - 1)).strftime("%Y-%m-%d") + " 24:00",
+            "genre": "all",
+            "limit": 500,
+            "offset": 0,
+            "startdatetime": today.strftime("%Y-%m-%d") + " 00:00",
+        }
+        channeldict = {x["channelid"]: x for x in self.__get("/live/epgs", params=params)["list"]}
 
         for idx, _ch in enumerate(self.req_channels):
             # 채널이름은 그대로 들어오고 프로그램 제목은 escape되어 들어옴
@@ -74,70 +129,19 @@ class WAVVE(EPGProvider):
             log.info("%03d/%03d %s", idx + 1, len(self.req_channels), _ch)
             for program in srcChannel["list"]:
                 try:
-                    _prog = EPGProgram(_ch.id)
-                    _prog.stime = datetime.strptime(program["starttime"], "%Y-%m-%d %H:%M")
-                    _prog.etime = datetime.strptime(program["endtime"], "%Y-%m-%d %H:%M")
-                    _prog.title = unescape(program["title"])
-                    matches = self.title_regex.match(_prog.title)
-                    if matches:
-                        _prog.title = (matches.group(1) or "").strip()
-                        _prog.title_sub = (matches.group(4) or "").strip()
-                        episode = (matches.group(2) or "").replace("회", "").strip()
-                        _prog.ep_num = "" if episode == "0" else episode
-                        _prog.rebroadcast = bool(matches.group(3))
-                    _prog.rating = 0 if program["targetage"] == "n" else int(program["targetage"])
-
-                    # 추가 정보 가져오기
-                    if self.cfg["GET_MORE_DETAILS"]:
-                        programid = program["programid"].strip()
-                        if programid and (programid not in programcache):
-                            # 개별 programid가 없는 경우도 있으니 체크해야함
-                            programdetail = self.get_program_details(programid)
-                            if programdetail is not None:
-                                programdetail["hit"] = 0  # to know cache hit rate
-                            programcache[programid] = programdetail
-
-                        if (programid in programcache) and bool(programcache[programid]):
-                            programcache[programid]["hit"] += 1
-                            programdetail = programcache[programid]
-                            # programtitle = programdetail['programtitle']
-                            # log.info('%s / %s' % (programName, programtitle))
-                            _prog.desc = "\n".join(
-                                [x.replace("<br>", "\n").strip() for x in programdetail["programsynopsis"].splitlines()]
-                            )  # carriage return(\r) 제거, <br> 제거
-                            _prog.category = programdetail["genretext"].strip()
-                            _prog.poster_url = "https://" + programdetail["programposterimage"].strip()
-                            # tags = programdetail['tags']['list'][0]['text']
-                            if programdetail["actors"]["list"]:
-                                _prog.actors = [x["text"] for x in programdetail["actors"]["list"]]
-                    _ch.programs.append(_prog)
+                    _prog = self.__program(_ch.id, program)
                 except Exception:
-                    log.exception("파싱 에러: %s", program)
+                    log.exception("개별 프로그램 가져오는 중 예외: %s", program)
+                else:
+                    _ch.programs.append(_prog)
             if not lazy_write:
                 _ch.to_xml(self.cfg, no_endtime=self.no_endtime)
 
-    def get_program_details(self, programid):
-        url = "https://apis.wavve.com/vod/programs-contentid/" + programid
-        referer = "https://www.wavve.com/player/vod?programid=" + programid
-        param = {
-            "apikey": "E5F3E0D30947AA5440556471321BB6D9",
-            "credential": "none",
-            "device": "pc",
-            "drm": "wm",
-            "partner": "pooq",
-            "pooqzone": "none",
-            "region": "kor",
-            "targetage": "auto",
-        }
-        self.sess.headers.update({"User-Agent": ua, "Referer": referer})
-
-        ret = None
+    @lru_cache
+    def get_program_details(self, programid: str):
         try:
-            contentid = request_data(url, params=param)["contentid"].strip()
-
-            url2 = 'https://apis.wavve.com/cf/vod/contents/' + contentid # 둘 사이를 왔다갔다 하는 모양이네요 지금은 여기만 됩니다.
-            # url2 = "https://apis.wavve.com/vod/contents/" + contentid  
-            ret = self.request(url2, params=param)
+            contentid = self.__get(f"/vod/programs-contentid/{programid}")["contentid"].strip()
+            return self.__get(f"/cf/vod/contents/{contentid}")
         except Exception:
-            log.exception("Exception while requesting data for %s with %s", url2, param)
-        return ret
+            log.exception("프로그램 상세 정보 요청 중 예외: %s", programid)
+            return None
