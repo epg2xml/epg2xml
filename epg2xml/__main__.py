f32a5994886d0ac730fdbe8081be16621de21eec
epg2xml <epg3xml@gmail.com>
2023-11-23 18:08:22 +0900

    EPGHandler를 도입해 여러 제공자를 통합 관리

--- epg2xml/__main__.py
+++ epg2xml/__main__.py
@@ -1,13 +1,10 @@
 import logging
 import socket
 import sys
-from collections import Counter
-from concurrent.futures import ThreadPoolExecutor
 from contextlib import ExitStack
 
-from epg2xml import __title__, __version__
 from epg2xml.config import Config
-from epg2xml.providers import load_channels, load_providers
+from epg2xml.providers import EPGHandler
 
 ############################################################
 # INIT
@@ -29,7 +26,7 @@ log = logging.getLogger("MAIN")
 
 def main():
     log.debug("Loading providers...")
-    providers = load_providers(conf.configs)
+    h = EPGHandler(conf.configs)
 
     if conf.args["cmd"] == "run":
         with ExitStack() as stack:
@@ -42,42 +39,20 @@ def main():
                 sys.stdout = stack.enter_context(sock.makefile("w"))
 
             log.debug("Loading service channels...")
-            load_channels(providers, conf.settings["channelfile"], conf.settings["parallel"])
+            h.load_channels(conf.settings["channelfile"], conf.settings["parallel"])
 
             log.debug("Loading requested channels...")
-            for p in providers:
-                p.load_req_channels()
-
-            log.debug("Checking uniqueness of channelid...")
-            cids = [c.id for p in providers for c in p.req_channels]
-            assert len(cids) == len(set(cids)), f"채널ID 중복: { {k:v for k,v in Counter(cids).items() if v > 1} }"
+            h.load_req_channels()
 
             log.debug("Getting EPG...")
-            if conf.settings["parallel"]:
-                with ThreadPoolExecutor() as exe:
-                    for p in providers:
-                        exe.submit(p.get_programs)
-            else:
-                for p in providers:
-                    p.get_programs()
+            h.get_programs(conf.settings["parallel"])
 
             log.info("Writing xmltv.dtd header...")
-            print('<?xml version="1.0" encoding="UTF-8"?>')
-            print('<!DOCTYPE tv SYSTEM "xmltv.dtd">\n')
-            print(f'<tv generator-info-name="{__title__} v{__version__}">')
-            stack.callback(print, "</tv>")
-
-            log.debug("Writing channels...")
-            for p in providers:
-                p.write_channels()
-
-            log.debug("Writing programs...")
-            for p in providers:
-                p.write_programs()
+            h.to_xml()
 
             log.info("Done")
     elif conf.args["cmd"] == "update_channels":
-        load_channels(providers, conf.settings["channelfile"], conf.settings["parallel"])
+        h.load_channels(conf.settings["channelfile"], conf.settings["parallel"])
     else:
         raise NotImplementedError(f"Unknown command: {conf.args['cmd']}")
 
