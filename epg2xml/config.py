dde8181105f8779b10f003be442a54720cd3ab46
epg2xml <epg3xml@gmail.com>
2026-04-04 02:44:03 +0900

    config: make argv metadata extensible
    - store argv spellings as lists in base_settings for future short-option expansion
    - unpack argv metadata directly into argparse.add_argument()
    - keep settings resolution priority as arg -> env -> default while preserving falsy edge handling
    - simplify command help text construction with a joined multiline tuple

--- epg2xml/config.py
+++ epg2xml/config.py
@@ -105,56 +105,56 @@ class Config:
 
     base_settings = {
         "config": {
-            "argv": "--config",
+            "argv": ["--config"],
             "env": "EPG2XML_CONFIG",
             "default": str(Path.cwd().joinpath("epg2xml.json")),
             "help": "config file path",
             "argparse": {"nargs": "?", "const": None},
         },
         "logfile": {
-            "argv": "--logfile",
+            "argv": ["--logfile"],
             "env": "EPG2XML_LOGFILE",
             "default": None,
             "help": "log file path",
             "argparse": {"nargs": "?", "const": None},
         },
         "loglevel": {
-            "argv": "--loglevel",
+            "argv": ["--loglevel"],
             "env": "EPG2XML_LOGLEVEL",
             "default": "INFO",
             "help": "loglevel",
             "argparse": {"choices": ("DEBUG", "INFO", "WARNING", "ERROR")},
         },
         "channelfile": {
-            "argv": "--channelfile",
+            "argv": ["--channelfile"],
             "env": "EPG2XML_CHANNELFILE",
             "default": str(Path.cwd().joinpath("Channel.json")),
             "help": "channel file path",
             "argparse": {"nargs": "?", "const": None},
         },
         "xmlfile": {
-            "argv": "--xmlfile",
+            "argv": ["--xmlfile"],
             "env": "EPG2XML_XMLFILE",
             "default": None,
             "help": "write output to file if specified",
             "argparse": {"nargs": "?", "const": None},
         },
         "xmlsock": {
-            "argv": "--xmlsock",
+            "argv": ["--xmlsock"],
             "env": "EPG2XML_XMLSOCK",
             "default": None,
             "help": "send output to unix socket if specified",
             "argparse": {"nargs": "?", "const": None},
         },
         "parallel": {
-            "argv": "--parallel",
+            "argv": ["--parallel"],
             "env": "EPG2XML_PARALLEL",
             "default": False,
             "help": "run in parallel",
             "argparse": {"action": "store_true"},
         },
         "dbfile": {
-            "argv": "--dbfile",
+            "argv": ["--dbfile"],
             "env": "EPG2XML_DBFILE",
             "default": None,
             "help": "export/import data to/from db",
@@ -270,21 +270,16 @@ class Config:
         )
         logging.getLogger().setLevel(initial_loglevel)
         for name, data in self.base_settings.items():
-            # Argrument priority: cmd < environment < default
+            # setting priority: arg -> env -> default
             try:
-                # Command line argument
-                arg_value = self.args.get(name)
-                if arg_value not in (None, False):
-                    value = arg_value
-                    logger.debug("setting from ARG   --%s=%s", name, arg_value)
+                if (value := self.args.get(name)) not in (None, False):
+                    logger.debug("setting from ARG   %s=%s", data["argv"][-1], value)
                 elif data["env"] in os.environ:
                     value = os.environ[data["env"]]
-                    logger.debug("setting from ENV   --%s=%s", data["env"], value)
-
-                # Default
+                    logger.debug("setting from ENV   %s=%s", data["env"], value)
                 else:
                     value = data["default"]
-                    logger.debug("setting by default %s=%s", data["argv"], value)
+                    logger.debug("setting by default %s=%s", name, value)
 
                 setts[name] = value
 
@@ -326,8 +321,12 @@ class Config:
             "cmd",
             metavar="command",
             choices=("run", "fromdb", "update_channels"),
-            help=(
-                '"run": XML 형식으로 출력\n' '"fromdb": dbfile로부터 불러오기\n' '"update_channels": 채널 정보 업데이트'
+            help="\n".join(
+                (
+                    '"run": XML 형식으로 출력',
+                    '"fromdb": dbfile로부터 불러오기',
+                    '"update_channels": 채널 정보 업데이트',
+                )
             ),
         )
 
@@ -344,7 +343,7 @@ class Config:
             if data["default"]:
                 help_text += f" (default: {data['default']})"
             kwargs = {"dest": name, "help": help_text, **data.get("argparse", {})}
-            parser.add_argument(data["argv"], **kwargs)
+            parser.add_argument(*data["argv"], **kwargs)
 
         # Print help by default if no arguments
         if len(sys.argv) == 1:
